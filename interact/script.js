12808df7b5211d8e297e3f8812a34b716e5e2316
Anshu <80555498+ProRaiden@users.noreply.github.com>
2023-05-25 03:03:04 +0530

    Delete interact directory

--- interact/script.js
+++
@@ -1,8 +0,0 @@
-function processInput() {
-    const userInput = document.getElementById('input').value;
-    const outputElement = document.getElementById('output');
-    // Process the input
-    const processedInput = userInput.toUpperCase();
-    // Display the output
-    outputElement.textContent = 'https://dashboard.heroku.com/new?template=' + processedInput;
-}
